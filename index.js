837b1f5593ecda2499e4e08b39ebd18986c5c0c0
Freddie Carthy <fcarthy@gmail.com>
2015-02-11 21:57:45 -0800

    mongo

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ server.register([
 //  { register: require('bell') },
 //  { register: require('hapi-auth-cookie') },
   { register: require('./app/www') },
-  { register: require('./app/api') },
-  //{ register: require('./app/db'), options: { url: process.env.MONGODB_URL || 'mongodb://localhost:27017/oscars' } },
+  //{ register: require('./app/api') },
+  { register: require('./app/db'), options: { url: process.env.MONGODB_URL || 'mongodb://localhost:27017/oscars' } },
 //  { register: require('good'), options: options },
 
 ], function(err) {
