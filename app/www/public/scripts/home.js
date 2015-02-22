a12b5c03bc09af11e7c78cf89d10650ee380b93c
Freddie Carthy <fcarthy@gmail.com>
2015-02-21 23:25:54 -0800

    Vote hover styles

--- app/www/public/scripts/home.js
+++ app/www/public/scripts/home.js
@@ -8,7 +8,7 @@ var getTallies = function() {
       $('.tallies ul').html('');
       for (var i = 0; k = tallies.length, i < k; i++) {
         var color = tallies[i].color || '#E0E0E0';
-        $('.tallies ul').append('<li><span class="user-mark" style="background-color: '+color+'"></span>'+tallies[i].name+': '+tallies[i].tally+'</li>');
+        $('.tallies ul').append('<li data-user="'+tallies[i].id+'"><span class="user-mark" style="background-color: '+color+'"></span>'+tallies[i].name+': '+tallies[i].tally+'</li>');
       }
     },
     error: function(response) {
@@ -58,6 +58,40 @@ $(".clear-winner").on("click", function() {
 
 $(".categories--category h2").on("click", function() {
   $(this).parent().toggleClass('active');
+});
+
+
+var prevTallyUser = null;
+$(".tallies").on("click", 'li', function() {
+  var user = $(this).data('user');
+  if (prevTallyUser != user) {
+    $(this).parent().find('li').addClass('inactive');
+    $(this).removeClass('inactive');
+    $('.nominee--votes a').addClass('inactive');
+    $('.nominee--votes').find('[data-user="'+user+'"]').removeClass('inactive');
+    $('.nominee--favorites a').addClass('inactive');
+    $('.nominee--favorites').find('[data-user="'+user+'"]').removeClass('inactive');
+    prevTallyUser = user;
+  } else {
+    $(this).parent().find('li').removeClass('inactive');
+    $('.nominee--votes a').removeClass('inactive');
+    $('.nominee--favorites a').removeClass('inactive');
+    prevTallyUser = null;
+  }
+});
+
+$('.tallies').on("mouseenter", "li", function() {
+  var user = $(this).data('user');
+  $('.nominee--votes a').addClass('hoverInactive');
+  $('.nominee--votes').find('[data-user="'+user+'"]').removeClass('hoverInactive');
+  $('.nominee--favorites a').addClass('hoverInactive');
+  $('.nominee--favorites').find('[data-user="'+user+'"]').removeClass('hoverInactive');
+});
+
+$('.tallies').on("mouseleave", "li", function() {
+  var user = $(this).data('user');
+  $('.nominee--votes a').removeClass('hoverInactive');
+  $('.nominee--favorites a').removeClass('hoverInactive');
 });
 
 $(".tallies").hover(function() {
