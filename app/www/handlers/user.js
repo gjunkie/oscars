b1341af204c6ef3668a5870586dbcaa93fd22677
Freddie Carthy <fcarthy@gmail.com>
2016-02-28 12:46:28 -0800

    Add in deadline for 2016

--- app/www/handlers/user.js
+++ app/www/handlers/user.js
@@ -22,15 +22,13 @@ exports.vote = {
           }
         }
       ],
-      /*
       before: function(request, reply, data) {
         var date = new Date();
-        var deadline = new Date('Wed Feb 22 2015 17:00:00 GMT-0800 (PST)');
+        var deadline = new Date('Wed Feb 28 2016 17:30:00 GMT-0800 (PST)');
         if (date > deadline) {
           return reply.redirect('/');
         }
       },
-      */
       view: 'vote'
     }
   }
